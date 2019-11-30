cb7961628486cc1afc796ec51ad49a2a2dced9fe
Maxime TELLENE <maxime.tellene@alumni.univ-avignon.fr>
2019-11-30 15:35:48 +0100

    ajout classe flibustier
    variables nom & armes
    methodes const & attaque

--- src/Flibustier.java
+++ src/Flibustier.java
@@ -1,2 +1,27 @@
-public class Flibustier implements Pirate {
+public class Flibustier implements Pirate{
+
+    //variables
+    String Nom;
+    String Arme;
+
+    //methodes
+    public Flibustier() {
+        String Nom = "Flibustier";
+        String Arme = "Mousquet";
+        System.out.println("Création d'un" +Nom+ "avec" +Arme);
+    }
+
+    @Override
+    public void Attaque() {
+        /*
+        if case_corsaire = case_flibustier+x
+        elseif case_corsaire = case_flibustier+y
+        elseif case_corsaire = case_flibustier-x
+        elseif case_corsaire = case_flibustier-y
+        elseif case_corsaire = case_flibustier+x+y
+        elseif case_corsaire = case_flibustier-x+y
+        elseif case_corsaire = case_flibustier+x-y
+        elseif case_corsaire = case_flibustier-x-y
+         */
+    }
 }
