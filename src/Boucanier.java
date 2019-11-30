5151fc7c0a01d60e9356525bc37caa8c36451777
Maxime TELLENE <maxime.tellene@alumni.univ-avignon.fr>
2019-11-30 15:34:18 +0100

    ajout interface Pirate
    methode attaque()

--- src/Boucanier.java
+++ src/Boucanier.java
@@ -1,2 +1,18 @@
 public class Boucanier implements Pirate {
+
+    //variables
+    String Nom;
+    String Arme;
+
+    //methodes
+    public Boucanier() {
+        String Nom = "Boucanier";
+        String Arme = "Machette";
+        System.out.println("Création d'un" +Nom+ "avec" +Arme);
+    }
+
+    @Override
+    public void Attaque() {
+        //if case_corsaire = case_boucanier
+    }
 }
