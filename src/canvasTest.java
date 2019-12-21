4cd05b4f8059db7f947e8f95553f9d43c95c0ce1
Nader SASSI <nader_84@live.fr>
2019-12-21 23:13:48 +0100

    Canvas adapted to a matrix second try

--- src/canvasTest.java
+++ src/canvasTest.java
@@ -3,6 +3,7 @@ import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import java.util.Random;
 
@@ -12,7 +13,11 @@ public class canvasTest extends Application {
         for (int i = 0 ; i < size ; i++){
             for (int j=0 ; j< size ; j++){
                 if (M[i][j] == 'x'){
-                    gc.strokeRect(j*20,i*20,20,20);
+                    gc.setFill(Color.BLUE);
+                    gc.fillRect(j*20,i*20,20,20);
+                }else if(M[i][j] == 'a'){
+                    gc.setFill(Color.GREEN);
+                    gc.fillRect(j*20,i*20,20,20);
                 }
                 System.out.print(M[i][j]);
             }
@@ -49,6 +54,7 @@ public class canvasTest extends Application {
         // Create the Pane
         Pane root = new Pane();
 
+        root.setStyle("-fx-background-color: LemonChiffon ");
         // Add the Canvas to the Pane
         root.getChildren().add(canvas);
 
@@ -57,6 +63,15 @@ public class canvasTest extends Application {
         for (int i=0; i < 10 ; i++){
             Random random = new Random();
             matrix[random.nextInt(15)][random.nextInt(15)] = 'x' ;
+        }
+        for (int i=0; i < 5 ; i++){
+            Random random = new Random();
+            int radomI, radomJ;
+            do {
+                radomI = random.nextInt(15);
+                radomJ = random.nextInt(15);
+            }while(matrix[radomI][radomJ] == 'x');
+            matrix[radomI][radomJ] = 'a';
         }
         displayMatrix(gc,matrix,15);
 
@@ -70,7 +85,7 @@ public class canvasTest extends Application {
         // Add the Scene to the Stage
         stage.setScene(scene);
         // Set the Title of the Stage
-        stage.setTitle("Creation of a Canvas");
+        stage.setTitle("LosPiratosDelTresore");
         // Display the Stage
         stage.show();
 
