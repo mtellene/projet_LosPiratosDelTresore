c0f061182351c4121a6ace7cf1933805a2975415
Maxime TELLENE <maxime.tellene@alumni.univ-avignon.fr>
2019-12-22 14:52:44 +0100

    création classe case

---
+++ src/Case.java
@@ -0,0 +1,15 @@
+public class Case {
+
+    public String type;
+    public int x;
+    public int y;
+    public boolean estVisible;
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY(){
+        return y;
+    }
+}
