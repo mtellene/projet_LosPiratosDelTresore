bad794ed9ef0c3e16df563928519a97b0a613910
soufiane abdelmoula <abdelmoulasoufiane97@gmail.com>
2019-12-27 00:37:15 +0100

    Fonctionnement Button Quitter : Success

--- home/src/gui/HomePage.java
+++ home/src/gui/HomePage.java
@@ -1,35 +1,62 @@
 package gui;
 import javafx.application.*;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.*;
 import javafx.stage.*;
-public class HomePage extends Application {
+public class HomePage extends Application implements EventHandler<ActionEvent>{
+	//creation du button
+	Button button1 = new Button("JOUER");
+	Button button2 = new Button("Recharger");
+	Button button3 = new Button("Quitter");
 
+	@Override
+	public void start(Stage FirstPage) throws Exception {
+		//Titre de la fenetre
+		FirstPage.setTitle("Les Piratos");
+		BorderPane borderPaneRoot = new BorderPane();
+		//la création de la scene
+		Scene pirate = new Scene (borderPaneRoot,500,300);
+		//Ajouter scene pirate au Stage
+		FirstPage.setScene(pirate);	
+		VBox Box1= new VBox();
+		Box1.setPadding(new Insets(10));
+		Box1.setSpacing(10);
+		button1.setPrefSize(180, 20);
+		button1.setOnAction(new EventHandler<ActionEvent>() {
+
+			@Override
+			public void handle(ActionEvent event) {
+				
+			}
+			
+		});
+		//ajouter action sur button2 pour quitter lapp
+		button3.setOnAction(new EventHandler<ActionEvent>() {
+
+			@Override
+			public void handle(ActionEvent t) {
+				System.exit(0);
+			}
+			
+		});
+		button2.setPrefSize(180, 20);
+		button3.setPrefSize(180, 20);
+		//ajouter les button au conteneur
+		Box1.getChildren().addAll(button1,button2,button3);
+		borderPaneRoot.setTop(Box1);
+		FirstPage.show();
+	}
 	public static void main(String[] args) {
 		launch(args);
 
 	}
-
 	@Override
-	public void start(Stage FirstPage) throws Exception {
-		FirstPage.setTitle("Les Piratos");
-		BorderPane borderPaneRoot = new BorderPane();
-		Scene pirate = new Scene (borderPaneRoot,500,300);
-		FirstPage.setScene(pirate);	
-		HBox hBox1= new HBox();
-		hBox1.setPadding(new Insets(10));
-		hBox1.setSpacing(10);
-		Button button1 = new Button("JOUER");
-		button1.setPrefSize(180, 20);
-		Button button2 = new Button("Recharger");
-		button2.setPrefSize(180, 20);
-		Button button3 = new Button("Quitter");
-		button3.setPrefSize(180, 20);
-		hBox1.getChildren().addAll(button1,button2,button3);
-		borderPaneRoot.setTop(hBox1);
-		FirstPage.show();
+	public void handle(ActionEvent event) {
+		
 	}
 
 }
